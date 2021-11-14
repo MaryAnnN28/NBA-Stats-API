86a3eb3c764e90ee997a0a82df109dd3a44ce8d7
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-11-14 12:40:30 -0500

    update to styling on landing page for game cards

--- src/components/Games/Games.js
+++ src/components/Games/Games.js
@@ -1,57 +1,67 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import "./Games.css";
-import Loader from "../Layout/Loader";
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import './Games.css'
+
+import { Box, Grid, Card, Container, Typography, makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles(() => ({
+	gameContainer: {
+		paddingTop: 10,
+		paddingBottom: 10,
+	},
+	gameCard: {
+		padding: 30,
+	},
+	score: {
+		fontSize: '24pt',
+		marginTop: '1rem',
+	},
+}))
 
 const Games = () => {
-  const [games, setGames] = useState([]);
-  const [loading, setLoading] = useState(false);
+	const classes = useStyles()
+	const [games, setGames] = useState([])
+	const [loading, setLoading] = useState(false)
 
-  useEffect(() => {
-    // const today = new Date().toISOString().slice( 0, 10 );
-    const today = new Date().toISOString().slice( 0, 10 );
+	useEffect(() => {
+		const today = new Date().toISOString().slice(0, 10)
 
-    setLoading(true);
-    axios.get(`https://www.balldontlie.io/api/v1/games?start_date=${today}&end_date=${today}`).then((game) => {
-      setGames(game.data.data);
-      setLoading(false);
-    })
-  }, []);
+		setLoading(true)
+		axios.get(`https://www.balldontlie.io/api/v1/games?start_date=${today}&end_date=${today}`).then((game) => {
+			setGames(game.data.data)
+			setLoading(false)
+		})
+	}, [])
 
-  const gamesList = games.map((game) => {
+	return (
+		<>
+			<Container>
+				<Grid container spacing={4} sx={{ mt: 10 }}>
+					{games.map((game) => (
+						<Grid item md={4} lg={4} xs={8} className={classes.gameContainer}>
+							<Card className={classes.gameCard}>
+								<Grid container spacing={4} sx={{ marginTop: 5, marginBottom: 5 }}>
+									<Grid item md={6} lg={6} sx={{ mt: 10 }}>
+										{game.home_team.abbreviation}
+										<div className={classes.score}>{game.home_team_score}</div>
+									</Grid>
+									<Grid item md={6} lg={6}>
+										{game.visitor_team.abbreviation}
+										<div className={classes.score}>{game.visitor_team_score}</div>
+									</Grid>
+								</Grid>
+								<Grid container spacing={4} sx={{ marginTop: 5, marginBottom: 5 }}>
+									<Grid item md={12} lg={12} sx={{ mt: 10 }}>
+										{game.status}
+									</Grid>
+								</Grid>
+							</Card>
+						</Grid>
+					))}
+				</Grid>
+			</Container>
+		</>
+	)
+}
 
-
-    return (
-      
-      <div key={game.id} className="ui card">
-        <div className="date-and-time">
-        <p className="team-vs-team-paragraph">
-          <span className="home-team-abv">{game.home_team.abbreviation}</span>
-          <span className="home-team-score">{game.home_team_score}</span>
-          <span>  </span>
-          <span className="visitor-team-abv">{game.visitor_team.abbreviation}</span>
-          <span className="visitor-team-score">{game.home_team_score}</span>
-        </p>
-          <h3 className="time-heading">{game.status}</h3>
-        </div>
-      </div>
-    );
-  });
-
-  return loading ? <Loader /> : (
-    <div className="games-container">
-      <h1
-        style={{
-          textAlign: "center",
-          marginTop: "2rem",
-          marginBottom: "1.5rem",
-        }}
-      >
-        Tonight's games
-      </h1>
-      {gamesList}
-    </div>
-  );
-};
-
-export default Games;
+export default Games
--- src/components/Layout/LandingPage.js
+++ src/components/Layout/LandingPage.js
@@ -1,17 +1,19 @@
-import React from "react";
-import Games from '../Games/Games'; 
+import React from 'react'
+import Games from '../Games/Games'
+import { Grid, Container, Box, Typography, makeStyles } from '@material-ui/core'
 
-import "./LandingPage.css";
+import './LandingPage.css'
 
 const LandingPage = () => (
-  <div className="landing-page-container">
-    <div className="about">
-      <h1 className="about-heading">Welcome to NBA Stats</h1>
-      <p className="about-paragraph">
-        <Games />
-      </p>
-    </div>
-  </div>
-);
+	<div className="landing-page-container">
+		<div className="about">
+			<h1 className="about-heading">Welcome to NBA Stats</h1>
+			<p className="about-paragraph">
+				<h3>Tonight's Games</h3>
+				<Games />
+			</p>
+		</div>
+	</div>
+)
 
-export default LandingPage;
+export default LandingPage
